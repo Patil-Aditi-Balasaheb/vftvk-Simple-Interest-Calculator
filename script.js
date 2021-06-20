3b8bab63ef978f918923a7d6944f04e31138f6d9
Aditi Balasaheb Patil <52288839+Patil-Aditi-Balasaheb@users.noreply.github.com>
2021-06-20 17:33:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    if(principal!=""){
     var interest = principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="If you deposit <span style='background-color:yellow;'>"+principal+"</span>,\<br\>at an interest rate of <span style='background-color:yellow;'>"+rate+"%</span>\<br\>You will receive an amount of <span style='background-color:yellow;'>"+interest+"</span>,\<br\>in the year <span style='background-color:yellow;'>"+year+"</span>\<br\>";
+    }
+    else
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus;
+    }
 }
 function updateRate()
 {
